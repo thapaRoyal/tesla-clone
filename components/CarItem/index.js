1cc7fbda92faf67a3cfcf1ad79c623f60f5a6cb1
thapaRoyal <thaparoyal17@gmail.com>
2021-09-25 13:47:15 +0545

    props for button component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -14,7 +14,20 @@ const CarItem = () => {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Starting at $69,420</Text>
       </View>
-      <StyledButton />
+      <StyledButton
+        type="primary"
+        content={'Custom Order'}
+        onPress={() => {
+          console.warn('Custom Order was pressed!');
+        }}
+      />
+      <StyledButton
+        type="secondary"
+        content={'Existing Inventory'}
+        onPress={() => {
+          console.warn('Existing Inventory was pressed!');
+        }}
+      />
     </View>
   );
 };
--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { View, Text, Pressable } from 'react-native';
 import styles from './styles';
 
-const StyledButton = () => {
+const StyledButton = (props) => {
+  const { type, content, onPress } = props;
+
+  const backgroundColor = type === 'primary' ? '#171a20cc' : '#ffffffa6';
+  const textColor = type === 'primary' ? '#fff' : '#171a20';
+
   return (
     <View style={styles.container}>
       <Pressable
-        style={styles.button}
-        onPress={() => {
-          console.warn('hey there!');
-        }}
+        style={[styles.button, { backgroundColor }]}
+        onPress={() => onPress()}
       >
-        <Text style={styles.text}>Custom Order</Text>
+        <Text style={[styles.text, { color: textColor }]}>{content}</Text>
       </Pressable>
     </View>
   );
