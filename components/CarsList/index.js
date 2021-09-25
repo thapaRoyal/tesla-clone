3acfee33e937e306bc98ed6c761a5856c3b6676e
thapaRoyal <thaparoyal17@gmail.com>
2021-09-25 14:31:14 +0545

    flatlist snap effect

--- components/CarsList/index.js
+++ components/CarsList/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Dimensions } from 'react-native';
 import styles from './styles';
 import cars from './cars';
 import CarItem from '../CarItem';
@@ -7,7 +7,14 @@ import CarItem from '../CarItem';
 const CarsList = () => {
   return (
     <View style={styles.container}>
-      <FlatList data={cars} renderItem={({ item }) => <CarItem car={item} />} />
+      <FlatList
+        data={cars}
+        renderItem={({ item }) => <CarItem car={item} />}
+        snapToAlignment={'start'}
+        decelerationRate={'fast'}
+        snapToInterval={Dimensions.get('window').height}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 };
